149cca35ad12c4d15dba78017239aedccfbc9539
Mike Conroy <mikecon@outlook.com>
2024-09-21 17:43:06 +0100

    Add New function which defaults to latest version

--- salesforce/salesforce.go
+++ salesforce/salesforce.go
@@ -7,5 +7,27 @@ type Salesforce struct {
 	ConsumerKey    string
 	ConsumerSecret string
 	SecurityToken  string
-	ApiVersion     string
+	ApiVersionPath string
+}
+
+/*
+ *  Returns an instance of the Salesforce config - defaulting to the latest version of the API.
+ */
+func New(instance, username, password, securityToken, consumerKey, consumerSecret string) Salesforce {
+	sf := Salesforce{
+		InstanceUrl:    instance,
+		Username:       username,
+		Password:       password,
+		ConsumerKey:    consumerKey,
+		ConsumerSecret: consumerSecret,
+		SecurityToken:  securityToken,
+	}
+	version, _ := sf.GetLatestVersion()
+	sf.ApiVersionPath = version.Url
+	return sf
+}
+
+func (sf Salesforce) Authenticate() (string, error) {
+
+	return "", nil
 }
